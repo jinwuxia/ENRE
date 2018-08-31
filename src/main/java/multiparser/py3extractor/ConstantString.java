9d658f3040454de37bfd7dde0b59ea3750da32fb
j123kaishichufa <j123kaishichufa@github.com>
2018-08-31 16:08:52 -0400

    add builtin functions

--- src/main/java/multiparser/py3extractor/ConstantString.java
+++ src/main/java/multiparser/py3extractor/ConstantString.java
@@ -22,6 +22,8 @@ public class ConstantString {
     public static final String MAIN_NAME = "__main__";
     public static final String CLASS_METHOD_CLS_PARAMETER = "cls";
 
+    public static final String CUSTOM_PRE = "CUSTOM_PRE";
+
     public static final String COMMENT = "'''";
     public static final String STRING_LETERAL_1 = "\"";
     public static final String STRING_LETERAL_2  = "'";
@@ -84,5 +86,23 @@ public class ConstantString {
             "in",
             "raise"
     };
+
+
+    public static String[] BUILT_IN_FUNCTIONS = {
+            "abs",	"delattr",	"hash",	"memoryview",	"set",
+            "all",	"dict",	"help",	"min",	"setattr",
+            "any",	"dir",	"hex",	"next",	"slice",
+            "ascii",	"divmod",	"id",	"object",	"sorted",
+            "bin",	"enumerate",	"input",	"oct",	"staticmethod",
+            "bool",	"eval",	"int",	"open",	"str",
+            "breakpoint",	"exec",	"isinstance",	"ord",	"sum",
+            "bytearray",	"filter",	"issubclass",	"pow",	"super",
+            "bytes",	"float",	"iter",	"print",	"tuple",
+            "callable",	"format",	"len",	"property",	"type",
+            "chr",	"frozenset",	"list",	"range",	"vars",
+            "classmethod",	"getattr",	"locals",	"repr",	"zip",
+            "compile",	"globals",	"map",	"reversed",	"__import__",
+            "complex",	"hasattr",	"max",	"round"
+};
 
 }
