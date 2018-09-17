f41057555b1da17c2521e8263cec88852e5feaa3
jinwuxia <j123kaishichufa@163.com>
2018-09-17 15:38:42 -0400

    change util directory

---
+++ src/main/java/util/FileUtil.java
@@ -0,0 +1,48 @@
+package util;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class FileUtil {
+
+    private ArrayList<String> fileNameList = new ArrayList<String>();
+
+    public FileUtil(String dirName) {
+        RecursiveFindFile(dirName);
+    }
+
+    public void RecursiveFindFile(String dirName)
+    {
+        //System.out.println(dirName);
+        File dir = new File(dirName);
+        if(dir.isFile()) {
+            this.fileNameList.add(dir.getAbsolutePath());
+            return;
+        }
+        for (File eachFile : dir.listFiles())
+        {
+            if(eachFile.isFile())
+            {
+                this.fileNameList.add(eachFile.getAbsolutePath());
+            }
+            else if(eachFile.isDirectory())
+            {
+                RecursiveFindFile(eachFile.getAbsolutePath());
+            }
+        }
+    }
+
+
+    public ArrayList<String> getFileNameList(String suffix)
+    {
+        ArrayList<String> goFileNameList = new ArrayList<String>();
+        for(String fileName : this.fileNameList)
+        {
+            if(fileName.endsWith(suffix))
+            {
+                goFileNameList.add(fileName);
+            }
+        }
+        return goFileNameList;
+    }
+}
