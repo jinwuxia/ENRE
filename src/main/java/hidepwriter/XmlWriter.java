014a9266792d2f677f2bf92a2eb6603fd60b9990
jinwuxia <j123kaishichufa@163.com>
2018-09-18 11:33:19 -0400

    code writer by extracting the code from formator

---
+++ src/main/java/hidepwriter/XmlWriter.java
@@ -0,0 +1,31 @@
+package hidepwriter;
+
+import formator.fxml.XDepObject;
+import util.Configure;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class XmlWriter {
+
+    public void toXml(Object xDepObject)  {
+        Configure configure = Configure.getConfigureInstance();
+
+        try {
+            //XDepObject class 
+            JAXBContext jaxbContext = JAXBContext.newInstance(XDepObject.class);
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.marshal(xDepObject, new FileOutputStream(configure.getOutputXmlFile()));
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
