6a229feb854342981d10dab671c7c414220d3ba8
jinwuxia <j123kaishichufa@163.com>
2018-09-19 17:32:53 -0400

    add entity type judgment function

--- src/main/java/udr/SingleCollect.java
+++ src/main/java/udr/SingleCollect.java
@@ -1,5 +1,9 @@
 package udr;
 
+import sun.security.krb5.Config;
+import util.Configure;
+import util.Tuple;
+
 import java.util.ArrayList;
 
 public class SingleCollect {
@@ -25,6 +29,69 @@ public class SingleCollect {
     public void addEntity(AbsEntity entity) {
         entities.add(entity);
     }
+
+
+    public boolean isFolder(int id) {
+        if(id == -1) {
+            return false;
+        }
+        if (singleCollectInstance.getEntities().get(id) instanceof AbsFLDEntity) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFile(int id) {
+        if(id == -1) {
+            return false;
+        }
+        if(singleCollectInstance.getEntities().get(id) instanceof AbsFILEntity) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isClass(int id) {
+        if(id == -1) {
+            return false;
+        }
+        if(singleCollectInstance.getEntities().get(id) instanceof AbsCLSEntity) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFunction(int id) {
+        if(id == -1) {
+            return false;
+        }
+        if(singleCollectInstance.getEntities().get(id) instanceof AbsFUNEntity) {
+            return true;
+        }
+        return false;
+
+    }
+
+    public boolean isCaredEntity(int id) {
+        if(id == -1) {
+            return false;
+        }
+        if(isFolder(id)) {
+            return true;
+        }
+        if(isClass(id)) {
+            return true;
+        }
+        if(isFile(id)) {
+            return true;
+        }
+        if(isFunction(id)) {
+            return true;
+        }
+
+        return false;
+    }
+
 
 
 }
