5c5c37da5164fc5e2156c7edcbd84e8f037dd5e4
jinwuxia <j123kaishichufa@163.com>
2019-03-04 23:42:54 -0500

    filed/method Px statistic

--- src/main/java/writer/JsonWriter.java
+++ src/main/java/writer/JsonWriter.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import util.Configure;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class JsonWriter {
@@ -13,7 +14,14 @@ public class JsonWriter {
     public void toJson(Object depObject, String fileName) {
         ObjectMapper mapper = new ObjectMapper();
         try {
-            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(fileName), depObject);
+            String jsonText = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(depObject);
+            //
+            jsonText = jsonText.replace("schemaVersion", "@schemaVersion");
+            //mapper.writerWithDefaultPrettyPrinter().writeValue(new File(fileName), jsonText);
+            FileWriter outputfile = new FileWriter(fileName);
+            outputfile.write(jsonText);
+            outputfile.close();
+
 
             //String jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(depObject);
             //System.out.println(jsonString);
