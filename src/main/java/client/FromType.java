e810c4939a838be8741cf5601bd07ca6fa56acc0
ZDH <ahong_934@163.com>
2021-01-15 21:08:09 +0800

    --from-type extension

--- src/main/java/client/FromType.java
+++ src/main/java/client/FromType.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class FromType {
+    static String typeExtractor = "E:\\TypeExtractor\\main.py";
+    static String cvs2json = "E:\\TypeExtractor\\csv2json.py";
     static void workflow(String [] args) throws IOException {
 
         final String out_dir = args[5];
@@ -14,14 +16,15 @@ public class FromType {
         System.out.println("");
         File proj = new File(proj_dir);
         if(proj.exists()){
+            String merged = merge(proj_dir,stub_dir);
             String proj_path = proj.getAbsolutePath();
             ProcessBuilder builder = new ProcessBuilder(
                     "python",
-                    "E:\\TypeExtractor\\main.py",
+                    typeExtractor,
                     "-o",
                     out_dir,
-                    proj_path,
-                    stub_dir
+                    merged,
+                    proj_path
             );
             builder.redirectErrorStream(true);
             Process process = builder.start();
@@ -36,5 +39,39 @@ public class FromType {
         else{
 
         }
+        toJson(proj, out_dir);
+    }
+
+    private static void toJson(File proj, String out_dir) throws IOException {
+
+        ProcessBuilder builder = new ProcessBuilder(
+                "python",
+                cvs2json,
+                proj.getAbsolutePath(),
+                out_dir,
+                "result-of-"+proj.getName()+".json"
+        );
+        builder.start();
+    }
+
+    private static String merge(String proj_dir, String stub_dir) throws IOException{
+        ProcessBuilder builder = new ProcessBuilder(
+                "python",
+                typeExtractor,
+                stub_dir,
+                proj_dir,
+                "--merge"
+        );
+        File f = new File(proj_dir);
+        builder.redirectErrorStream(true);
+        Process process = builder.start();
+        BufferedReader r = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while (true) {
+            line = r.readLine();
+            if (line == null) { break; }
+            System.out.println(line);
+        }
+        return f.getName();
     }
 }
